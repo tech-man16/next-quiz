d5a4cf509ae1cfcd7786695dae013232d50910a8
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-08-30 20:40:57 +0530

    Update 2_Main.tsx

--- app/components/2_Main.tsx
+++ app/components/2_Main.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useEffect, useState } from "react";
 import { handle, search } from "@/app/server-actions/actions";
 import { RadioGroup, Radio } from "@nextui-org/radio";
