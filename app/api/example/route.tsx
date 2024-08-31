d124bc9c7add9e3201955648b3e9d87a95785108
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-08-31 10:51:01 +0530

    Update route.tsx

--- app/api/example/route.tsx
+++ app/api/example/route.tsx
@@ -1,1 +1,12 @@
+import path from "path";
+import { promises as fs } from "fs";
 
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest){
+    return NextResponse.json({message: "Fetched Successfully from get!!" , status:200},{status:200}) ;
+}
+
+export async function POST(req: NextRequest) {
+    return NextResponse.json({message: "Fetched Successfully from post !!" , status:200},{status:200}) ;
+}
