8ae7bbaab7d3e9e311c421e9afb812432eb13b65
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-08-31 16:57:39 +0530

    Update route.tsx

--- app/server-actions/getdata/route.tsx
+++ app/server-actions/getdata/route.tsx
@@ -8,7 +8,7 @@ export async function GET(req: NextRequest){
         const data = await fs.readFile(path.join(process.cwd(),"/app/files/MCQ-1.json"), "utf-8");
         const p_data = JSON.parse(data);
         //const p_data = {url: req.nextUrl.pathname} ;
-        return NextResponse.json(p_data, {status:200});
+        return NextResponse.json({ data:p_data, status:200 }, {status:200});
     } catch(e){
         return NextResponse.json({message: "Fetched Successfully !!" , status:500},{status:500}) ;
     }
