47153e871ffd2f5cd7ada6698aea914996426754
Manas Maheshwari <100331835+tech-man16@users.noreply.github.com>
2024-08-31 08:02:30 +0530

    Update route.tsx

--- app/server-actions/getdata/route.tsx
+++ app/server-actions/getdata/route.tsx
@@ -3,6 +3,10 @@ import { promises as fs } from "fs";
 
 import { NextRequest, NextResponse } from 'next/server';
 
+export async function GET() {
+  return NextResponse.json({message:"Success from get",status:200},{status:200})
+}
+
 export async function POST(req: NextRequest) {
     const data = await fs.readFile(path.join(process.cwd(), "app/files/MCQ-1.json"), "utf-8");
     const p_data = JSON.parse(data);
